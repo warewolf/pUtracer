c1881d51fd2685e1dd37638ba3b478ed4d254755
Richard Harman <richard@richardharman.com>
2018-03-05 01:10:43 -0500

    More work

--- decode.pl
+++ decode.pl
@@ -1,54 +1,142 @@
 #!/usr/bin/perl
-
+# vim: foldmethod=marker commentstring=\ #\ %s
 use strict;
 use warnings;
 use Data::Dumper;
+use List::Util qw(max);
 
-my $str = "100000000000000000002C002B033802870000";
+# Resistors in voltage dividers, 400V version
+use constant {   # {{{
+  AnodeR1 => 5_230,   # R32
+  AnodeR2 => 470_000, # R33
+  AnodeRs => 17.8,    # R45 ... actually 18 ohm, but we cheat w/ 17.8 for correction http://dos4ever.com/uTracerlog/tubetester2.html#examples
+  #
+  ScreenR1 => 5_230,   # R18
+  ScreenR2 => 470_000, # R19
+  ScreenRs => 17.8,    # R20 ... actually 18 ohm, but we cheat w/ 17.8 for correction http://dos4ever.com/uTracerlog/tubetester2.html#examples
+  #
+  VsupR1 => 1_800, # R44
+  VsupR2 => 6_800, # R43
+  #
+  VminR1 => 2_000, # R3 (-15v supply)
+  VminR2 => 47_000 # R4 (-15v supply)
+}; # }}}
+
+# XXX factor these aliases out?
+use constant { # {{{
+  AnodeDivR1 => AnodeR1,
+  AnodeDivR2 => AnodeR2,
+  ScreenDivR1 => ScreenR1,
+  ScreenDivR2 => ScreenR2,
+}; # }}}
+
+
+#my $str = "10 0000 0000 0000 0000 002C 002B 0338 0287 0000"; # from me
+my $str =  "10 077A 0000 0724 0001 0034 0033 0338 0288 0707"; # from utracer
+$str =~ s/ //g;
 my $data = {};
-@{$data}{qw(
+my @keys = qw(
   status_byte
-  anode_current_pga
-  anode_current_raw
-  screen_current_pga
-  screen_current_raw
-  anode_voltage
-  screen_volage
-  psu_voltage
-  negative_voltage
-  anode_pga_gain
-  screen_pga_gain
-)}=map {hex($_) } unpack("A2A4A4A4A4A4A4A4A4A2A2",$str);
+
+  Ia
+  Ia_Raw
+
+  Is
+  Is_Raw
+
+  Va
+  Vs
+  Vpsu
+  Vmin
+
+  Gain_A
+  Gain_S
+);
+@{$data}{@keys} = map {hex($_) } unpack("A2 A4 A4 A4 A4 A4 A4 A4 A4 A2 A2",$str);
+
+# status byte = 10 - all good.
+# status byte = 11 - compliance error
+
+# my calibration values (YMMV) 
+use constant { # {{{
+CalVar1  => 1018/1000, # Va Gain
+CalVar2  => 1008/1000, # Vs Gain
+CalVar3  =>  990/1000, # Ia Gain
+CalVar4  =>  985/1000, # Is Gain
+CalVar5  => 1011/1000, # VsupSystem
+CalVar6  => 1014/1000, # Vgrid(40V)
+CalVar7  => 1000/1000, # VglowA
+CalVar8  => 1018/1000, # Vgrid(4V)
+CalVar9  =>  996/1000, # Vgrid(sat)
+CalVar10 => 1000/1000, # VglowB, unused?
+}; # }}}
 
 # these magic scale constants came from another project, uTmax.
 use constant { # {{{
-  SCALE_IA => 1000.0*5.0/1023.0/18.0,
-  SCALE_IA_RAW => 1000.0*5.0/1023.0/18.0,
-  SCALE_IS => 1000.0*5.0/1023.0/18.0,
-  SCALE_IS_RAW => 1000.0*5.0/1023.0/18.0,
+  SCALE_IA     =>  5 / 1024 * 1000 / AnodeRs,
+  SCALE_IA_RAW =>  5 / 1024 * 1000 / AnodeRs,
   #
-  SCALE_IA_400 => 1000.0*5.0/1023.0/9.0,
-  SCALE_IA_400_RAW => 1000.0*5.0/1023.0/9.0,
-  SCALE_IS_400 => 1000.0*5.0/1023.0/9.0,
-  SCALE_IS_400_RAW => 1000.0*5.0/1023.0/9.0,
+  SCALE_IS     =>  5 / 1024 * 1000 / ScreenRs,
+  SCALE_IS_RAW =>  5 / 1024 * 1000 / ScreenRs,
   #
-  SCALE_VA => (470.0+5.23)/5.230, # oh, son of a bitch this is wrong. 6.8k is 300V version.  Should be 5.23
-  SCALE_VS => (470.0+5.230)/5.230, # oh, son of a bitch this is wrong. 6.8k is 300V version.  Should be 5.23
-  SCALE_VSU => 5.0/1023.0*(1.8+6.8)/1.8,
+  SCALE_VA => 5 / 1024 * ((AnodeDivR1 + AnodeDivR2) / AnodeDivR1),
+  SCALE_VS => 5 / 1024 * ((ScreenDivR1 + ScreenDivR2) / ScreenDivR1),
+
+  SCALE_VSU    => ( 5 / 1024 ) * (VsupR1+VsupR2)/VsupR1, # needs calibration scale
   SCALE_VN=> 49.0/2.0,
   SCALE_GAIN_A=>1,
   SCALE_GAIN_S => 1,
+}; # }}}
+
+my $gain_to_average = { # {{{
+  7 => 8,
+  6 => 4,
+  5 => 2,
+  4 => 2,
+  3 => 1,
+  2 => 1,
+  1 => 1,
+  1 => 1,
 }; # }}}
 
 use constant {
   HEAT_CNT_MAX => 20,
 };
 
-print "Scale VSU = ",SCALE_VSU,"\n";
-
 print "Before scale:\n";
-print Data::Dumper->Dump([$data],[qw($data)]);
-$data->{psu_voltage} *= SCALE_VSU;
-$data->{psu_voltage} *= 1.011; # calibration value
-print "\nAfter scale:\n";  
-print Data::Dumper->Dump([$data],[qw($data)]);
+printf "stat   ia iacmp   is  is_comp    va   vs vPSU vneg  ia_gain is_gain\n";
+printf "%04x %04x %04x  %04x  %04x     %04x %04x %04x %04x  %04x    %04x\n",
+        @{$data}{@keys};
+
+my $gain = max @{$data}{qw(Gain_A Gain_S)};
+
+$data->{Vpsu} *= SCALE_VSU;
+$data->{Vpsu} *= CalVar5;
+
+$data->{Va} = $data->{Va} * (5 / 1024) * ((AnodeDivR1 + AnodeDivR2) / (AnodeDivR1 * CalVar1)) - $data->{Vpsu};
+$data->{Vs} = $data->{Vs} * (5 / 1024) * ((ScreenDivR1 + ScreenDivR2) / (ScreenDivR1 * CalVar2)) - $data->{Vpsu};
+
+$data->{Ia} *= SCALE_IA;
+$data->{Ia} *= CalVar3;
+
+$data->{Is} *= SCALE_IS;
+$data->{Is} *= CalVar4;
+
+
+$data->{Vmin} = 5 * ((VminR1 + VminR2) / VminR1) * (( $data->{Vmin} / 1024) - 1);
+$data->{Vmin} += 5;
+
+
+
+$data->{Ia} /= $gain_to_average->{$gain};
+$data->{Is} /= $gain_to_average->{$gain};
+
+print "\nAfter scale:\n";
+printf "stat   ia iacmp   is  is_comp    va   vs vPSU vneg  ia_gain is_gain\n";
+printf "%04x %2.1f % 5.1f %2.1f      %2.1f   %2.1f  %2.1f %2.1f %2.1f  %04d    %04d\n",
+        @{$data}{@keys};
+
+printf "\nShould be:\n";
+printf "stat   ia iacmp   is  is_comp    va   vs vPSU vneg  ia_gain is_gain\n";
+printf "%04x %2.1f % 5.1f %2.1f      %2.1f   %2.1f  %2.1f %2.1f %2.1f  %04d    %04d\n",
+       0x10,65,0,61.7,0.3,3.2,3,19.4,-40,200,200;
--- point.pl
+++ point.pl
@@ -16,14 +16,21 @@ use constant {
   CMD_PING => "50"
 };
 
+# Resistors in voltage dividers, 400V version
 use constant {
-  AnodeR1 => 5230,
-  AnodeR2 => 470000,
-  AnodeRs => 17.8,
+  AnodeR1 => 5_230,   # R32
+  AnodeR2 => 470_000, # R33
+  AnodeRs => 17.8,    # R45 ... actually 18 ohm, but we cheat w/ 17.8 for correction http://dos4ever.com/uTracerlog/tubetester2.html#examples
   #
-  ScreenR1 => 5230,
-  ScreenR2 => 470000,
-  ScreenRs => 17.8,
+  ScreenR1 => 5_230,   # R18
+  ScreenR2 => 470_000, # R19
+  ScreenRs => 17.8,    # R20 ... actually 18 ohm, but we cheat w/ 17.8 for correction http://dos4ever.com/uTracerlog/tubetester2.html#examples
+  #
+  VsupR1 => 1_800, # R44
+  VsupR2 => 6_800, # R43
+  #
+  VminR1 => 2_000, # R3 (-15v supply)
+  VminR2 => 47_000 # R4 (-15v supply)
 };
 
 # XXX factor these aliases out?
@@ -34,9 +41,22 @@ use constant {
   ScreenDivR2 => ScreenR2,
 };
 
+# calculations to go to/fro stuff
 use constant { # {{{
-  SCALE_VSU  => (5/1023)*(1800+6800)/1800,
-  SCALE_IA => 5*1024,
+
+  SCALE_VSU    => ( 5 / 1024 ) * (VsupR1+VsupR2)/VsupR1, # needs calibration scale
+  # 
+  SCALE_IA     => ( 5 / 1024 ) * 1000 / AnodeRs, # needs calibration scale
+  SCALE_IA_PGA => ( 5 / 1024 ) * 1000 / AnodeRs, # needs calibration scale
+  #
+  SCALE_IS     => ( 5 / 1024 ) * 1000 / ScreenRs, # needs calibration scale
+  SCALE_IS_PGA => ( 5 / 1024 ) * 1000 / ScreenRs, # needs calibration scale
+  #
+  SCALE_VA     => ( 5 / 1024 ) * ((AnodeDivR1 + AnodeDivR2) / AnodeDivR1), # subtract supply voltage, needs calibration
+  SCALE_VS     => ( 5 / 1024 ) * ((ScreenDivR1 + ScreenDivR2) / ScreenDivR1), # subtract supply voltage, needs calibration
+  #
+  #SCALE_VN     =>  # I have no fucking clue how to do this, I don't think I can do the way I want to.
+  
 }; # }}}
 
 my $tubes = { # {{{
@@ -87,7 +107,7 @@ GetOptions($opts,"hot","device=s","tube=s","vg=f","va=i","vs=i","rp=f","ia=f","g
 delete $opts->{tube};
 
 # my calibration values (YMMV)
-my $CalVar1  = 1018/1000; #  Va Gain
+my $CalVar1  = 1018/1000; # Va Gain
 my $CalVar2  = 1008/1000; # Vs Gain
 my $CalVar3  =  990/1000; # Ia Gain
 my $CalVar4  =  985/1000; # Is Gain
@@ -96,17 +116,11 @@ my $CalVar6  = 1014/1000; # Vgrid(40V)
 my $CalVar7  = 1000/1000; # VglowA
 my $CalVar8  = 1018/1000; # Vgrid(4V)
 my $CalVar9  =  996/1000; # Vgrid(sat)
-my $CalVar10 = 1000/1000; # VglowB
+my $CalVar10 = 1000/1000; # VglowB, unused?
 
 # my PSU voltage (YMMV)
 my $VsupSystem=824*SCALE_VSU*$CalVar5;
 print "vsup = $VsupSystem\n";
-
-
-my ($VsupR1,$VSupR2) = (1800,6800);
-my ($ScreenDivR1,$ScreenDivR2)= (5230,470000); # XXX THESE ARE THE SAME AS ScreenR1 ScreenR2
-my ($AnodeDivR1,$AnodeDivR2)= (5230,470000); # XXX THESE ARE THE SAME AS AnodeR1 AnodeR2
-my ($VminR1, $VminR2) = (2000,47000); # negative supply
 
 my ($Ia,$IaComp) = (0,0); # Ia is anode current, IaComp is anode current before PGA
 my ($Is,$IsComp) = (0,0); # Is is screen current, IsComp is screen current before PGA
@@ -123,7 +137,15 @@ sub getVs {
 
 sub getVg {
   my ($voltage) = @_;
-  return ((-1024 * $voltage  * $CalVar6) / 50) + 0.00001
+  my $cal ;
+  if (abs($voltage) > 4) {
+    print STDERR "Using 40v calibration value\n";
+    $cal = $CalVar6;
+  } else {
+    print STDERR "Using 4v calibration value\n";
+    $cal = $CalVar8;
+  }
+  return ((-1023 * $voltage  * $cal) / 50) + 0.00001
 }
 
 sub getVf {
