01837bacb133be178e2941971d16bdb0bfabf8df
Richard Harman <richard@richardharman.com>
2018-03-05 22:30:35 -0500

    Okay, this works

--- decode.pl
+++ decode.pl
@@ -30,48 +30,21 @@ use constant { # {{{
   ScreenDivR2 => ScreenR2,
 }; # }}}
 
-
-#my $str = "10 0000 0000 0000 0000 002C 002B 0338 0287 0000"; # from me
-my $str =  "10 077A 0000 0724 0001 0034 0033 0338 0288 0707"; # from utracer
-$str =~ s/ //g;
-my $data = {};
-my @keys = qw(
-  status_byte
-
-  Ia
-  Ia_Raw
-
-  Is
-  Is_Raw
-
-  Va
-  Vs
-  Vpsu
-  Vmin
-
-  Gain_A
-  Gain_S
-);
-@{$data}{@keys} = map {hex($_) } unpack("A2 A4 A4 A4 A4 A4 A4 A4 A4 A2 A2",$str);
-
-# status byte = 10 - all good.
-# status byte = 11 - compliance error
-
-# my calibration values (YMMV) 
+# calibration
 use constant { # {{{
-CalVar1  => 1018/1000, # Va Gain
-CalVar2  => 1008/1000, # Vs Gain
-CalVar3  =>  990/1000, # Ia Gain
-CalVar4  =>  985/1000, # Is Gain
-CalVar5  => 1011/1000, # VsupSystem
-CalVar6  => 1014/1000, # Vgrid(40V)
-CalVar7  => 1000/1000, # VglowA
-CalVar8  => 1018/1000, # Vgrid(4V)
-CalVar9  =>  996/1000, # Vgrid(sat)
-CalVar10 => 1000/1000, # VglowB, unused?
+  CalVar1  => 1018/1000, # Va Gain
+  CalVar2  => 1008/1000, # Vs Gain
+  CalVar3  =>  990/1000, # Ia Gain
+  CalVar4  =>  985/1000, # Is Gain
+  CalVar5  => 1011/1000, # VsupSystem
+  CalVar6  => 1014/1000, # Vgrid(40V)
+  CalVar7  => 1000/1000, # VglowA, unused?
+  CalVar8  => 1018/1000, # Vgrid(4V)
+  CalVar9  =>  996/1000, # Vgrid(sat)
+  CalVar10 => 1000/1000, # VglowB, unused?
 }; # }}}
 
-# these magic scale constants came from another project, uTmax.
+# scale constants
 use constant { # {{{
   SCALE_IA     =>  5 / 1024 * 1000 / AnodeRs,
   SCALE_IA_RAW =>  5 / 1024 * 1000 / AnodeRs,
@@ -83,10 +56,27 @@ use constant { # {{{
   SCALE_VS => 5 / 1024 * ((ScreenDivR1 + ScreenDivR2) / ScreenDivR1),
 
   SCALE_VSU    => ( 5 / 1024 ) * (VsupR1+VsupR2)/VsupR1, # needs calibration scale
-  SCALE_VN=> 49.0/2.0,
-  SCALE_GAIN_A=>1,
-  SCALE_GAIN_S => 1,
+  SCALE_VN     => 49.0/2.0, # this is confusing.
+  HEAT_CNT_MAX => 20,
 }; # }}}
+
+my @measurement_fields = qw(
+    status_byte
+
+    Ia
+    Ia_Raw
+
+    Is
+    Is_Raw
+
+    Va
+    Vs
+    Vpsu
+    Vmin
+
+    Gain_A
+    Gain_S
+);
 
 my $gain_to_average = { # {{{
   7 => 8,
@@ -99,44 +89,77 @@ my $gain_to_average = { # {{{
   1 => 1,
 }; # }}}
 
-use constant {
-  HEAT_CNT_MAX => 20,
+my $gain_from_tracer = {
+  0 => 1,
+  1 => 2,
+  2 => 5,
+  3 => 10,
+  4 => 20,
+  5 => 50,
+  6 => 100,
+  7 => 200,
 };
 
-print "Before scale:\n";
-printf "stat   ia iacmp   is  is_comp    va   vs vPSU vneg  ia_gain is_gain\n";
-printf "%04x %04x %04x  %04x  %04x     %04x %04x %04x %04x  %04x    %04x\n",
-        @{$data}{@keys};
+#my $data = decode_measurement("10 077A 0000 0724 0001 0034 0033 0338 0288 0707"); # from utracer
+my $data = decode_measurement("10 02B0 0043 02B4 0044 01DB 01DA 033A 0287 0303"); # from utracer
 
-my $gain = max @{$data}{qw(Gain_A Gain_S)};
+sub decode_measurement {
+  my ($str) = @_;
+  $str =~ s/ //g;
+  my $data = {};
+  @{$data}{@measurement_fields} = map {hex($_) } unpack("A2 A4 A4 A4 A4 A4 A4 A4 A4 A2 A2",$str);
 
-$data->{Vpsu} *= SCALE_VSU;
-$data->{Vpsu} *= CalVar5;
+  # status byte = 10 - all good.
+  # status byte = 11 - compliance error
+  # $compliance_error = $status_byte & 0x1 == 1;
 
-$data->{Va} = $data->{Va} * (5 / 1024) * ((AnodeDivR1 + AnodeDivR2) / (AnodeDivR1 * CalVar1)) - $data->{Vpsu};
-$data->{Vs} = $data->{Vs} * (5 / 1024) * ((ScreenDivR1 + ScreenDivR2) / (ScreenDivR1 * CalVar2)) - $data->{Vpsu};
+  # find max gain
+  my $gain = max @{$data}{qw(Gain_A Gain_S)};
 
-$data->{Ia} *= SCALE_IA;
-$data->{Ia} *= CalVar3;
+  $data->{Vpsu} *= SCALE_VSU;
+  $data->{Vpsu} *= CalVar5;
 
-$data->{Is} *= SCALE_IS;
-$data->{Is} *= CalVar4;
+  $data->{Va} = $data->{Va} * (5 / 1024) * ((AnodeDivR1 + AnodeDivR2) / (AnodeDivR1 * CalVar1)) - $data->{Vpsu};
+  $data->{Vs} = $data->{Vs} * (5 / 1024) * ((ScreenDivR1 + ScreenDivR2) / (ScreenDivR1 * CalVar2)) - $data->{Vpsu};
 
+  $data->{Ia} *= SCALE_IA;
+  $data->{Ia} *= CalVar3;
 
-$data->{Vmin} = 5 * ((VminR1 + VminR2) / VminR1) * (( $data->{Vmin} / 1024) - 1);
-$data->{Vmin} += 5;
+  $data->{Is} *= SCALE_IS;
+  $data->{Is} *= CalVar4;
 
+  $data->{Ia_Raw} *= SCALE_IA;
+  $data->{Ia_Raw} *= CalVar3;
 
+  $data->{Is_Raw} *= SCALE_IS;
+  $data->{Is_Raw} *= CalVar4;
 
-$data->{Ia} /= $gain_to_average->{$gain};
-$data->{Is} /= $gain_to_average->{$gain};
+  $data->{Vmin} = 5 * ((VminR1 + VminR2) / VminR1) * (( $data->{Vmin} / 1024) - 1);
+  $data->{Vmin} += 5;
+
+  # decode gain
+  @{$data}{qw(Gain_A Gain_S)} = map { $gain_from_tracer->{$_} } @{$data}{qw(Gain_A Gain_S)};
+
+  $data->{Ia} /= $gain_to_average->{$gain};
+  $data->{Is} /= $gain_to_average->{$gain};
+
+  # correction - this appears to be backwards?
+  if (0) {
+    #$data->{Va} -= (($data->{Ia}) / 1000) * AnodeRs - (0.6 * CalVar7);
+    #$data->{Vs} -= (($data->{Is}) / 1000) * ScreenRs - (0.6 * CalVar7);
+    
+    $data->{Va} = $data->{Va} - (($data->{Ia}) / 1000) * AnodeRs - (0.6 * CalVar7);
+    $data->{Vs} = $data->{Vs} - (($data->{Is}) / 1000) * ScreenRs - (0.6 * CalVar7);
+  }
 
 print "\nAfter scale:\n";
 printf "stat   ia iacmp   is  is_comp    va   vs vPSU vneg  ia_gain is_gain\n";
 printf "%04x %2.1f % 5.1f %2.1f      %2.1f   %2.1f  %2.1f %2.1f %2.1f  %04d    %04d\n",
-        @{$data}{@keys};
+        @{$data}{@measurement_fields};
 
 printf "\nShould be:\n";
 printf "stat   ia iacmp   is  is_comp    va   vs vPSU vneg  ia_gain is_gain\n";
 printf "%04x %2.1f % 5.1f %2.1f      %2.1f   %2.1f  %2.1f %2.1f %2.1f  %04d    %04d\n",
        0x10,65,0,61.7,0.3,3.2,3,19.4,-40,200,200;
+return $data;
+}
